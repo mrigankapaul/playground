06390b4de6e617e11456dddacf7415be76545d07
mriganka.paul <paul.mriganka@gmail.com>
2020-11-23 15:30:09 -0500

    Promise Explored

---
+++ 8-promise.js
@@ -0,0 +1,39 @@
+// const doWorkPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve([1, 4, 7])
+//     reject("This is my error");
+//   }, 2000);
+// });
+
+// doWorkPromise
+//   .then((result) => {
+//     console.log("Success", result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+// add(1,2).then((sum) => {
+//   console.log(sum)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+add(1, 2)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 5);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  }).catch((e) => {
+      console.log(e)
+  })
